9bf6f4803417cf1659fc2b4d3ae6dcba24a3c7c9
Eleftherios Pournos-Georgiou <lefos@MacBook-Pro-Eleftherios.local>
2020-11-16 14:53:31 +0200

    final bootstrap

--- app.py
+++ app.py
@@ -38,9 +38,10 @@ def index():
 @app.route('/posts', methods=['GET','POST'])
 def posts():
     if request.method== 'POST':
+        post_author = request.form['author']
         post_title = request.form['title']
         post_content = request.form['content']
-        new_post = BlogPost(title=post_title, content=post_content,author='Lefos')
+        new_post = BlogPost(title=post_title, content=post_content,author=post_author)
         db.session.add(new_post)
         db.session.commit()
         return redirect('/posts')
@@ -52,7 +53,24 @@ def posts():
 def hello(name,id):
     return f'Hello {name}, your id is: {id}'
 
+@app.route('/posts/delete/<int:id>')
+def delete(id):
+    post = BlogPost.query.get_or_404(id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/posts')
 
+@app.route('/posts/edit/<int:id>', methods=['GET','POST'])
+def edit(id):
+    post = BlogPost.query.get_or_404(id)
+    if request.method=='POST':
+        post.title = request.form['title']
+        post.content= request.form['content']
+        post.author= request.form['author']
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        return render_template('edit.html', post=post)
 
 
 if __name__ == '__main__':
